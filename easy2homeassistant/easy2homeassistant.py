2560b3cf02feced5dbbf1898c1e7e13a04006370
Thomas Steiner <47025493+steinerthomas@users.noreply.github.com>
2024-11-06 22:23:52 +0100

    Parser for temperature sensors (#5)
    * settings.json test config
    * Parse Products.xml
    * Remove TODO and add sensor to README
    * Parser for temperature sensors

--- easy2homeassistant/easy2homeassistant.py
+++ easy2homeassistant/easy2homeassistant.py
@@ -91,6 +91,7 @@ class EntityKind(Enum):
     UNDEFINED = 0
     LIGHT = 1
     COVER = 2
+    TEMPERATURE_SENSOR = 3
 
     def __str__(self):
         return f"{self.name} ({self.value})"
@@ -106,6 +107,10 @@ class Light:
     state_address: int = 0
     brightness_state_address: Optional[int] = None
 
+    def is_valid(self):
+        """Check if the entity is valid."""
+        return self.name != "" and self.address != 0 and self.state_address != 0
+
 
 @dataclass
 class Cover:
@@ -119,6 +124,32 @@ class Cover:
     position_state_address: int = 0
     angle_state_address: int = 0
 
+    def is_valid(self):
+        """Check if the entity is valid."""
+        return (
+            self.name != ""
+            and self.move_long_address != 0
+            and self.stop_address != 0
+            and self.position_address != 0
+            and self.angle_address != 0
+            and self.position_state_address != 0
+            and self.angle_state_address != 0
+        )
+
+
+@dataclass
+class TemperatureSensor:
+    """A data class to represent a temperature sensor entity."""
+
+    name: str
+    state_address: int = 0
+    type: str = "temperature"
+    state_class: str = "measurement"
+
+    def is_valid(self):
+        """Check if the entity is valid."""
+        return self.name != "" and self.state_address != 0
+
 
 @dataclass
 class Entities:
@@ -126,6 +157,7 @@ class Entities:
 
     light: List = field(default_factory=list)
     cover: List = field(default_factory=list)
+    sensor: List = field(default_factory=list)
 
     def add_entity(self, entity):
         """Add an entity to the corresponding list."""
@@ -134,6 +166,8 @@ class Entities:
             self.light.append(entity)
         elif isinstance(entity, Cover):
             self.cover.append(entity)
+        elif isinstance(entity, TemperatureSensor):
+            self.sensor.append(entity)
         else:
             logger.critical("Invalid entity '%s'", entity)
 
@@ -155,20 +189,25 @@ class XMLParser:
         "Slat angle control": "angle_address",
         "Position control status": "position_state_address",
         "Slat angle control status": "angle_state_address",
+        # sensor
+        "Indoor temperature": "state_address",
     }
 
     def __init__(self):
         self.entities = Entities()
         self.entity = None  # currently parsed entity
         self.address_attribute_name = None  # currently parsed address
+        self.products = {}
 
     def add_entity(self):
         """Add the current entity to the entities list."""
-        if self.entity is not None:
+        added = False
+        if self.entity is not None and self.entity.is_valid():
             self.entities.add_entity(self.entity)
-            self.entity = None
-        else:
-            logger.error("Empty entity occurred!")
+            added = True
+
+        self.entity = None
+        return added
 
     def parse_group_addresses(self, group_addresses):
         """Parse group addresses and set the lowest address to the currently parsed entity."""
@@ -190,7 +229,11 @@ class XMLParser:
                 lowest_address,
             )
         else:
-            logger.error("No group address found!")
+            logger.info(
+                "'%s': No group address for '%s'",
+                self.entity.name,
+                self.address_attribute_name,
+            )
 
     def parse_datapoints(self, datapoints):
         """Parse datapoints and map them to the corresponding entity attribute."""
@@ -216,6 +259,34 @@ class XMLParser:
                             datapoint_name,
                         )
 
+    def parse_context(self, context):
+        """Parse a context element and set the entity name."""
+        if self.entity.name != "":
+            logger.debug(
+                "The entity name is already set: '%s'. Skip parsing name by serial number!",
+                self.entity.name,
+            )
+            return
+
+        serial_number = ""
+        for prop in context.findall("property"):
+            if prop.get("key") == "product.serialNumber":
+                serial_number = prop.get("value")
+                break
+
+        if serial_number in self.products:
+            self.entity.name = self.products[serial_number]
+            logger.info(
+                "Set entity name to '%s' by serial number '%s'.",
+                self.entity.name,
+                serial_number,
+            )
+        else:
+            logger.error(
+                "No product name found for serial number '%s'.",
+                serial_number,
+            )
+
     def parse_config(self, config):
         """Generic parser for a config element."""
         if config is None:
@@ -223,7 +294,7 @@ class XMLParser:
             return
 
         name = config.get("name")
-        if name in ("Context", "Parameters"):
+        if name in ("Parameters"):
             logger.debug("Skip '%s'", name)
             return
         if name == "datapoints":
@@ -231,6 +302,9 @@ class XMLParser:
             return
         if name == "groupAddresses":
             self.parse_group_addresses(config)
+            return
+        if name == "Context":
+            self.parse_context(config)
             return
 
         if name == "FunctionalBlocks" or name.lstrip("-").isdigit():
@@ -254,27 +328,33 @@ class XMLParser:
         for prop in channel.findall("property"):
             if prop.get("key") == "Name":
                 name = prop.get("value")
-                if name == "":
-                    logger.debug("Skip unnamed channel: %s", channel.get("name"))
             elif prop.get("key") == "Icon":
-                if prop.get("value") == "icon-shutter":
+                icon = prop.get("value")
+                if icon == "icon-shutter":
                     kind = EntityKind.COVER
-                else:
+                elif icon in ("icon-light", "icon-dimmer"):
                     kind = EntityKind.LIGHT
+                elif icon == "icon-indoor_temperature":
+                    kind = EntityKind.TEMPERATURE_SENSOR
 
-        if name != "" and kind != EntityKind.UNDEFINED:
+        if kind != EntityKind.UNDEFINED:
+            if kind in (EntityKind.COVER, EntityKind.LIGHT) and name == "":
+                logger.debug("Skip unnamed channel of kind %s", kind)
+                return
+
             # create entity and search for attributes
             if kind is EntityKind.COVER:
                 self.entity = Cover(name)
             elif kind is EntityKind.LIGHT:
                 self.entity = Light(name)
+            elif kind is EntityKind.TEMPERATURE_SENSOR:
+                self.entity = TemperatureSensor(name)
 
             logger.info("Found entity '%s' of kind %s", name, kind)
             for config in channel.findall("config"):
                 self.parse_config(config)
-            self.add_entity()
 
-    def parse_channel_xml(self, channels_xml):
+    def parse_channels_xml(self, channels_xml):
         """Parse the Channels.xml file and return the entities."""
         logger.info("Parsing xml file '%s'", channels_xml)
 
@@ -283,8 +363,40 @@ class XMLParser:
 
         for channel in root.findall("config"):
             self.parse_channel(channel)
+            if not self.add_entity():
+                logger.debug("Skip unnamed channel: %s", channel.get("name"))
 
         return self.entities
+
+    def parse_product(self, product):
+        """Parse a product element. Store the name for the serial number."""
+        name = ""
+        serial_number = ""
+        for prop in product.findall("property"):
+            if prop.get("key") == "SerialNumber":
+                serial_number = prop.get("value")
+                if serial_number == "":
+                    logger.warning(
+                        "Found product without SerialNumber: %s", product.get("name")
+                    )
+            elif prop.get("key") == "product.name":
+                name = prop.get("value")
+
+        if serial_number != "":
+            logger.info(
+                "Found product '%s' with serial number '%s'", name, serial_number
+            )
+            self.products[serial_number] = name
+
+    def parse_products_xml(self, products_xml):
+        """Parse the Products.xml file."""
+        logger.info("Parsing xml file '%s'", products_xml)
+
+        tree = ET.parse(products_xml)
+        root = tree.getroot()
+
+        for product in root.findall("config"):
+            self.parse_product(product)
 
 
 def parse_arguments():
@@ -314,6 +426,16 @@ def parse_arguments():
     return arg_parser.parse_args()
 
 
+def get_configuration_xml_file(project_dir, file_name):
+    """Get the path to an xml file in the temporary directory."""
+    xml_file = os.path.join(project_dir, "configuration", file_name)
+    if not os.path.exists(xml_file):
+        logger.error("%s not found in the extracted files.", xml_file)
+        return None
+
+    return xml_file
+
+
 def main():
     """Main function to extract and convert an easy project to a HomeAssistant configuration."""
     args = parse_arguments()
@@ -326,13 +448,17 @@ def main():
         with zipfile.ZipFile(args.input, "r") as zip_ref:
             zip_ref.extractall(temp_dir)
 
-        channels_xml_file = os.path.join(temp_dir, "configuration", "Channels.xml")
-        if not os.path.exists(channels_xml_file):
-            logger.error("%s not found in the extracted files.", channels_xml_file)
+        channels_xml_file = get_configuration_xml_file(temp_dir, "Channels.xml")
+        if channels_xml_file is None:
+            return
+
+        products_xml_file = get_configuration_xml_file(temp_dir, "Products.xml")
+        if products_xml_file is None:
             return
 
         parser = XMLParser()
-        entities = parser.parse_channel_xml(channels_xml_file)
+        parser.parse_products_xml(products_xml_file)
+        entities = parser.parse_channels_xml(channels_xml_file)
 
         yaml_configuration = args.output
         logger.info("Exporting entities to '%s'", yaml_configuration)
