e0d30b9bf331790bc97dfa96062071c78c1a316d
Thomas Steiner <thomas.steiner94@gmail.com>
2025-03-27 23:05:41 +0100

    climate (Temperature controller) (#13)
    * ignore projects
    * Documentation for climate (temperature controllers)
    * Test data for climate
    * climate parsing
    * refactor parser

--- tests/test_easy_parser.py
+++ tests/test_easy_parser.py
@@ -4,10 +4,17 @@ import unittest
 import yaml
 
 from easy_parser import XMLParser
+from homeassistant_entities import convert_project_to_entities
 
 
 class TestXMLParser(unittest.TestCase):
     """Tests for XMLParser."""
+
+    def setUp(self):
+        """Set up the test case."""
+
+        # pylint: disable=invalid-name
+        self.maxDiff = None  # Allow full diff output for assertions
 
     def test_xml_parser_channels_xml(self):
         """Test the XMLParser with a Channels.xml and Products.xml file."""
@@ -18,7 +25,10 @@ class TestXMLParser(unittest.TestCase):
         expected_output_file = "tests/resources/output.yaml"
 
         parser.parse_products_xml(products_xml_file)
-        entities = parser.parse_channels_xml(channels_xml_file)
+        parser.parse_channels_xml(channels_xml_file)
+        project = parser.get_project()
+
+        entities = convert_project_to_entities(project)
 
         with open(expected_output_file, "r", encoding="utf-8") as f:
             expected_output = yaml.safe_load(f)
