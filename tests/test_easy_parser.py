57038568d4c13eb49fc3189d69de540d6ec87df0
Thomas Steiner <thomas.steiner94@gmail.com>
2025-03-27 22:17:56 +0100

    adjust tests

--- tests/test_easy_parser.py
+++ tests/test_easy_parser.py
@@ -4,6 +4,7 @@ import unittest
 import yaml
 
 from easy_parser import XMLParser
+from homeassistant_entities import convert_project_to_entities
 
 
 class TestXMLParser(unittest.TestCase):
@@ -18,7 +19,10 @@ class TestXMLParser(unittest.TestCase):
         expected_output_file = "tests/resources/output.yaml"
 
         parser.parse_products_xml(products_xml_file)
-        entities = parser.parse_channels_xml(channels_xml_file)
+        parser.parse_channels_xml(channels_xml_file)
+        project = parser.get_project()
+
+        entities = convert_project_to_entities(project)
 
         with open(expected_output_file, "r", encoding="utf-8") as f:
             expected_output = yaml.safe_load(f)
